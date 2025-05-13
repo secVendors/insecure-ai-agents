48b77d69b0bee4af1403528a075b558f40da9c1c
Allie Howe <allie@secvendors.cmo>
2025-05-13 10:14:07 -0400

    single agent example

---
+++ mastra/single-agent/src/mastra/index.ts
@@ -0,0 +1,18 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { createLogger } from '@mastra/core/logger';
+import { LibSQLStore } from '@mastra/libsql';
+
+import { weatherAgent, movePlannerAgent } from './agents';
+
+export const mastra = new Mastra({
+  agents: { weatherAgent, movePlannerAgent },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: createLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+});
